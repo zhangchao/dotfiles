49bf6b585e77ec9b993756389b233711632dcf90
Zhang Chao <super.chao.zhang@gmail.com>
2017-09-29 10:39:08 +0800

    Create mac_install.sh

---
+++ mac_install.sh
@@ -0,0 +1,23 @@
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ 
+# http://braumeister.org/formula
+brew install wget
+brew install curl
+brew install git
+brew install maven@3.3
+brew install tmux
+brew install vim
+brew install python3
+brew install ipython
+ 
+# https://caskroom.github.io/search
+brew tap caskroom/cask
+# https://caskroom.github.io/search
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install virtualbox
+brew cask install intellij-idea-ce
+brew cask install docker-toolbox
+brew cask install sublime-text
+brew cask install kdiff3
